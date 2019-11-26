60e67ae080d016ed5c6e9140acf561ba97a838fb
Christopher Haster <chaster@utexas.edu>
2019-11-26 16:42:49 -0600

    Fixed implicit change-of-sign warning in lfs_dir_fetch
    Warning on MDK v5.27.1
    Found by geniusgogo

--- lfs.c
+++ lfs.c
@@ -977,7 +977,8 @@ static int lfs_dir_fetch(lfs_t *lfs,
         lfs_mdir_t *dir, const lfs_block_t pair[2]) {
     // note, mask=-1, tag=-1 can never match a tag since this
     // pattern has the invalid bit set
-    return (int)lfs_dir_fetchmatch(lfs, dir, pair, -1, -1, NULL, NULL, NULL);
+    return (int)lfs_dir_fetchmatch(lfs, dir, pair,
+            (lfs_tag_t)-1, (lfs_tag_t)-1, NULL, NULL, NULL);
 }
 
 static int lfs_dir_getgstate(lfs_t *lfs, const lfs_mdir_t *dir,
