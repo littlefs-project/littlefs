a942cdba664dd9f7b48dd7494807c29ef6fe313c
Christopher Haster <geky@geky.net>
2023-06-30 00:28:10 -0500

    Bumped minor version to v2.7

--- lfs.h
+++ lfs.h
@@ -21,7 +21,7 @@ extern "C"
 // Software library version
 // Major (top-nibble), incremented on backwards incompatible changes
 // Minor (bottom-nibble), incremented on feature additions
-#define LFS_VERSION 0x00020006
+#define LFS_VERSION 0x00020007
 #define LFS_VERSION_MAJOR (0xffff & (LFS_VERSION >> 16))
 #define LFS_VERSION_MINOR (0xffff & (LFS_VERSION >>  0))
 
