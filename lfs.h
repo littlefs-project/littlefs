1d68133f0f6b11f654b954f9410003bfc24524be
crafcat7 <crafcat7@outlook.com>
2024-04-30 16:12:59 +0800

    lfs.c:Support get file path
    Signed-off-by: crafcat7 <crafcat7@outlook.com>

--- lfs.h
+++ lfs.h
@@ -656,6 +656,10 @@ int lfs_file_rewind(lfs_t *lfs, lfs_file_t *file);
 // Returns the size of the file, or a negative error code on failure.
 lfs_soff_t lfs_file_size(lfs_t *lfs, lfs_file_t *file);
 
+// Get the absolute path of the open file.
+//
+// Returns a negative error code on failure.
+int lfs_file_path(lfs_t *lfs, lfs_file_t *file, char *path, lfs_size_t size);
 
 /// Directory operations ///
 
@@ -706,6 +710,10 @@ lfs_soff_t lfs_dir_tell(lfs_t *lfs, lfs_dir_t *dir);
 // Returns a negative error code on failure.
 int lfs_dir_rewind(lfs_t *lfs, lfs_dir_t *dir);
 
+// Get the absolute path of the directory
+//
+// Returns a negative error code on failure.
+int lfs_dir_path(lfs_t *lfs, lfs_dir_t *dir, char *path, lfs_size_t size);
 
 /// Filesystem-level filesystem operations
 
