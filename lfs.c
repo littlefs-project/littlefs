d5a86fd28de74dd1d5a6126d843b16276e6023cf
ryancw <1831931681@qq.com>
2025-06-03 09:46:59 +0800

    style: format code, limit to 80 columns.

--- lfs.c
+++ lfs.c
@@ -5225,7 +5225,9 @@ static int lfs_fs_gc_(lfs_t *lfs) {
     }
 
     // try to populate the lookahead buffer, unless it's already full
-    if (lfs->lookahead.size < lfs_min(8 * lfs->cfg->lookahead_size, lfs->block_count)) {
+    if (lfs->lookahead.size < lfs_min(
+            8 * lfs->cfg->lookahead_size,
+            lfs->block_count)) {
         err = lfs_alloc_scan(lfs);
         if (err) {
             return err;
