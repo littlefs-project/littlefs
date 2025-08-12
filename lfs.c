11cecd079c26c4a220f250440b310814e6c9fe27
Joakim Plate <joakim.plate@rt-labs.com>
2025-08-12 16:29:31 +0200

    fix: also assert inside lfs_bd_read

--- lfs.c
+++ lfs.c
@@ -93,6 +93,7 @@ static int lfs_bd_read(lfs_t *lfs,
             // bypass cache?
             diff = lfs_aligndown(diff, lfs->cfg->read_size);
             int err = lfs->cfg->read(lfs->cfg, block, off, data, diff);
+            LFS_ASSERT(err <= 0);
             if (err) {
                 return err;
             }
