9699faac5e2fffa9fc57f13480f1a81acdc65a07
Marco Nilsson <marco@zyax.se>
2025-09-18 15:29:47 +0200

    Change include style from "" to <>.
    This allows using libraries to override headers in order to inject things like
    custom logging function definitions.

--- lfs.c
+++ lfs.c
@@ -5,8 +5,8 @@
  * Copyright (c) 2017, Arm Limited. All rights reserved.
  * SPDX-License-Identifier: BSD-3-Clause
  */
-#include "lfs.h"
-#include "lfs_util.h"
+#include <lfs.h>
+#include <lfs_util.h>
 
 
 // some constants used throughout the code
--- lfs_util.c
+++ lfs_util.c
@@ -5,7 +5,7 @@
  * Copyright (c) 2017, Arm Limited. All rights reserved.
  * SPDX-License-Identifier: BSD-3-Clause
  */
-#include "lfs_util.h"
+#include <lfs_util.h>
 
 // Only compile if user does not provide custom config
 #ifndef LFS_CONFIG
